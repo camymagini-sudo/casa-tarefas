f88e380a943b536ce9b7666db8c26e8af6c9279c
camymagini-sudo <camymagini@gmail.com>
2026-01-06 18:24:21 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main style={{ padding: 16 }}>
-      <h1>Casa - Tarefas</h1>
-      <Link href="/login">Ir para login</Link>
-    </main>
-  );
+  redirect("/app");
 }
